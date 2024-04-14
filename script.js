8fab9bbfe8584d2157ff8fa0a05a7417910b4b4f
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-04-14 20:56:47 +0200

    ajout module

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+import express from "express";
+
+
+
 const nav = document.querySelector('.navbar')
 fetch('/navbar.html')
 .then(res=>res.text())
 .then(data=>{
     nav.innerHTML=data
 })
+
+express.Router();
+
