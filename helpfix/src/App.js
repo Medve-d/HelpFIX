68fff6d7e947c3c05f01c36fcf426c3ff3430d6e
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-04-23 21:46:09 +0200

    suppression contenu CSS

--- helpfix/src/App.js
+++ helpfix/src/App.js
@@ -1,39 +1,46 @@
 import './App.css';
-import {createBrowserRouter, Link, RouterProvider} from 'react-router-dom';
+import {createBrowserRouter, Link, RouterProvider, Outlet, NavLink} from 'react-router-dom';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter ([
   {
-    path: "/",
-    element: <div>Page d'accueil
-      <nav>
-        <Link to ="/">Accueil</Link>
-        <Link to ="/blog">Blog</Link>
-        <Link to ="/contact">Contact</Link>
-      </nav>
-    </div>,
-    
-  },
-  {
-    path: "/blog",
-    element: <div>Bienvenue sur le blog
-      <nav>
-        <Link to ="/">Accueil</Link>
-        <Link to ="/blog">Blog</Link>
-        <Link to ="/contact">Contact</Link>
-      </nav>
-    </div>,
-  },
-  {
-    path: "/contact",
-    element: <div>Bienvenue sur la page contact
-      <nav>
-        <Link to ="/">Accueil</Link>
-        <Link to ="/blog">Blog</Link>
-        <Link to ="/contact">Contact</Link>
-      </nav>
-    </div>,
-  },
+    path : '/',
+    element: <Root/>,
+    errorElement: <PageError/>,
+    children: [
+      {
+        path: 'signin',
+        element: <div>S'inscrire</div>,
+      },
+      {
+        path: 'login',
+        element: <div>Se connecter</div>,
+      },
+    ]
+  }
 ]);
+
+function PageError() {
+  const error = UseRouteError()
+  console.log(error)
+  return <>
+    <h1>Une erreur est survenue</h1>
+  </>
+}
+
+function Root () {
+  return <>
+    <header>
+      <nav>
+        <NavLink to="/">Accueil</NavLink>
+        <NavLink to="/signin">S'inscrire</NavLink>
+        <NavLink to="/login">Se connecter</NavLink>
+      </nav>
+    </header>
+    <div className='container my-4'>
+      <Outlet/>
+    </div>
+  </>
+}
 
 function App() {
   return <RouterProvider router={router}/>
