1212757225802981939df9ec672ca7699076c838
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-06-15 15:05:38 +0200

    Format liste de prestation

--- helpfix/components/RequestList.jsx
+++ helpfix/components/RequestList.jsx
@@ -3,20 +3,20 @@ import RequestItem from './RequestItem';
 import styles from '../styles/RequestList.module.css';
 
 const requests = [
-  { name: "Paul", message: "Réparer machine à laver", date: "12/12/2025" },
+  { name: "Paul",date: "12/12/2025" , message: "Réparer machine à laver" },
   //Pour back-end select les infos ici 
 ];
 
 const RequestList = () => {
   return (
     <div className={styles.requestList}>
-      <h2>En Attente</h2>
+      <h2>Liste des prestations demandées</h2>
       {requests.map((request, index) => (
         <RequestItem
           key={index}
           name={request.name}
-          message={request.message}
           date={request.date}
+          message={request.message}
         />
       ))}
     </div>
