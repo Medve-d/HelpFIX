2362bf07093be7722ab3e040a82c3ab4a9281f37
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-07-05 10:36:26 +0200

    Changement service et accueil

--- frontend/pages/category.jsx
+++ frontend/pages/category.jsx
@@ -1,12 +1,33 @@
 import Category from "@/components/Category";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/footComp";
-
+import Image from "next/image";
 
 export default function categoryPage() {
+    const images = [
+        { src: '/images/photo_ville_paris.jpg', alt: 'Image Paris', name: 'Paris' },
+        { src: '/images/photo_ville_marseille.jpg', alt: 'Image Marseille', name: 'Marseille' },
+        { src: '/images/photo_ville_lyon.jpg', alt: 'Image Lyon', name:'Lyon' },
+        { src: '/images/photo_ville_lille.jpg', alt: 'image Lille', name:'Lille' },
+        { src: '/images/photo_ville_bordeaux.jpg', alt: 'Image Bordeaux', name:'Bordeaux' },
+        { src: '/images/photo_ville_nice.jpg', alt: 'Image Nice', name:'Nice' },
+    ];
     return <>
     <Navbar />
-    <Category />
+    <section className="section section-categoHome">
+  <div className="Acard-container">
+    {images.map((image, index) => (
+      <a href="#" className="Acard" key={index}>
+        <div className="img-container">
+          <Image src={image.src} alt={image.alt} width={300} height={300}  />
+          <div className="overlay">
+            <div className="text-Card">{image.name}</div>
+          </div>
+        </div>
+      </a>
+    ))}
+  </div>
+</section>
     <Footer />
     </>
 }
