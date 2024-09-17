c94e663281f023936b9a3d5731300a6eb9f966b6
AnasElmaliki69 <anaselmaliki0@gmail.com>
2024-09-17 09:54:17 +0200

    db config fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,9 @@
+// backend/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI;
-    if (!uri) {
-      throw new Error('MONGO_URI is not defined');
-    }
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // Timeout pour la sélection du serveur
-    });
+    await mongoose.connect(process.env.MONG_URI);
     console.log('MongoDB connected successfully.');
   } catch (error) {
     console.error('Connection to database failed: ' + error.message);
