7f84a7b69eb93a57b4474185f7e3e53d7eae9f57
AnasElmaliki69 <anaselmaliki0@gmail.com>
2024-09-21 15:06:42 +0200

    ajouts de responsive Nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthContext } from './hooks/useAuthContext'
 
 // pages & components
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
