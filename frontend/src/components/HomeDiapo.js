7707dd26c32e5ec4e3ecf765e05b1b2ca9494f8b
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2025-03-07 11:58:34 +0100

    Image home-diapo

--- frontend/src/components/HomeDiapo.js
+++ frontend/src/components/HomeDiapo.js
@@ -1,35 +1,23 @@
 import React from 'react';
 import { useAuthContext } from '../hooks/useAuthContext';
-import Image from 'next/image';
 
 const HomeDiapo = () => {
     const { user } = useAuthContext();
-    const images = process.env.NEXT_PUBLIC_URL 
-        ? [
-              `${process.env.NEXT_PUBLIC_URL}/image/jardinierDiapo.jpg`,
-          ]
-        : [];
-
     return (
         <div className='HomeDiapo'>
             <div className="HomeOverlay" />
             <div className="diaporama-container">
-                {images.length > 0 ? ( 
-                    images.map((image, index) => (
-                        <Image
-                            key={index}
-                            src={image}
-                            alt="Diaporama"
-                            className="diaporama-image"
-                            width={1920}
-                            height={1080}
-                            layout="fill"
-                            objectFit="cover"
+( 
+(
+                        <img
+                            src={`${process.env.PUBLIC_URL}/image/jardinierDiapo.jpg`}
+                            alt="diaporama"
+                            className="Home-image"
                         />
                     ))
-                ) : (
+                 : (
                     <div>Chargement des images...</div>
-                )}
+                )
             </div>
             <div className="HomeContent">
                 <h1>Votre solution à tous vos besoins de services,</h1>
